af73f6fd38b959f6d24754cc10d969a22a0786a0
Jacob W. S. Schneider <jacob.schneider@outlook.com>
2018-10-26 11:18:49 -0400

    Update halloween_sounds.py

--- halloween_sounds.py
+++ halloween_sounds.py
@@ -1,5 +1,7 @@
-import RPi.GPIO as GPIO                           #Import GPIO library
-import time                                       #Import time library
+# Oct. 22, 2018
+# Python program that plays random MP3 when IR sensor is triggered
+import RPi.GPIO as GPIO                           
+import time                                      
 import datetime
 import  subprocess
 import os
@@ -9,30 +11,30 @@ mp3Files=[]
 
 i = 0
 
-for root, dirs, files in os.walk("/home/pi/motionSensor/SpookySounds"):
+for root, dirs, files in os.walk("/home/pi/halloweenDisplay/SpookySounds"):
 	for filename in files:
 		mp3Files.append(filename)
 		i=i+1
 
-GPIO.setmode(GPIO.BOARD)                          #Set GPIO pin numbering
-pir = 26                                          #Associate pin 26 to pir
-GPIO.setup(pir, GPIO.IN)                          #Set pin as GPIO in 
+GPIO.setmode(GPIO.BOARD)                          #Bring up GPIO pins
+pir = 26                                          #Set to whichever pin is listening to the IR sensor
+GPIO.setup(pir, GPIO.IN)                          
 print "HALLOWEEN MUSIC - Waiting for sensor to settle"
-time.sleep(2)                                     #Waiting 2 seconds for the sensor to initiate
+time.sleep(2)                                     #Give the sensor 2 seconds to warm up
 print "Detecting motion"
-while True:
-   if GPIO.input(pir):                            #Check whether pir is HIGH
+while True:					  #Loop forever
+   if GPIO.input(pir):                             
       print "Motion Detected!"
       print (datetime.datetime.now())
 
       j = random.randint(0,i-1)
 
-      print "Playing: " + "/home/pi/motionSensor/SpookySounds/"+mp3Files[j]
+      print "Playing: " + "/home/pi/halloweenDisplay/SpookySounds/"+mp3Files[j]
 
-      p = subprocess.Popen(["omxplayer", "/home/pi/motionSensor/SpookySounds/"+mp3Files[j]], stdout=subprocess.PIPE)
+      p = subprocess.Popen(["omxplayer", "/home/pi/halloweenDisplay/SpookySounds/"+mp3Files[j]], stdout=subprocess.PIPE)
 
       print p.communicate()
 
-      time.sleep(2)                               #D1- Delay to avoid multiple detection
-   time.sleep(0.1)                                #While loop delay should be less than detection(hardware) delay
+      time.sleep(2)                               #Delay to avoid false, double detection
+   time.sleep(0.1)                               
 
