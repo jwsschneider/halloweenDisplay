32d390d9dfad154d71800b2c970f0929a9029cdb
Jacob W. S. Schneider <jacob.schneider@outlook.com>
2018-10-26 11:11:40 -0400

    Update halloween.sh

--- halloween.sh
+++ halloween.sh
@@ -1,4 +1,9 @@
+# Oct. 25,2018
+# Bash script that brings up the LED daemon and launches both the lights and sounds Python programs to run in the background. This script is called from the root's /etc/init.d/rc.local script at startup.
 #!/bin/bash
 
+# Launch LED daemon
 sudo pigpiod &
+
+# Launch Python programs
 python /home/pi/halloweenDisplay/halloween_sounds.py &> /home/pi/halloweenDisplay/log.out
