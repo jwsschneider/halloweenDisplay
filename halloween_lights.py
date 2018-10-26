4381ce48e2b139d049996bd26c44aaf95c13f93e
Jacob W. S. Schneider <jacob.schneider@outlook.com>
2018-10-26 11:16:30 -0400

    Update halloween_lights.py

--- halloween_lights.py
+++ halloween_lights.py
@@ -1,12 +1,15 @@
-import RPi.GPIO as GPIO                           #Import GPIO library
-import time                                       #Import time library
+# Oct. 24, 2018
+# Python program that flashes an LED strip RED, GREEN, PURPLE and ORANGE for 1 sec. each, with no repeats, on loop for 1 minute.
+
+import RPi.GPIO as GPIO                           
+import time                                     
 import subprocess
 import datetime
 import os
 import random
 
 
-
+# Series of functions that use command-line commands to set the RGB of the LED strip
 def purple():
 	subprocess.Popen(["pigs","p 27 0"], stdout=subprocess.PIPE) # R
 	subprocess.Popen(["pigs","p 17 255"], stdout=subprocess.PIPE) # G
@@ -27,6 +30,7 @@ def orange():
 	subprocess.Popen(["pigs","p 17 175"], stdout=subprocess.PIPE) # G
 	subprocess.Popen(["pigs","p 22 255"], stdout=subprocess.PIPE) # B
 
+# Helper function for debugging - sets RBG on LED strip to OFF
 def killLights():
 	subprocess.Popen(["pigs","p 27 255"], stdout=subprocess.PIPE) # R
 	subprocess.Popen(["pigs","p 17 255"], stdout=subprocess.PIPE) # G
@@ -34,14 +38,14 @@ def killLights():
 
 i = 0
 
-GPIO.setmode(GPIO.BOARD)                          #Set GPIO pin numbering
-pir = 26                                          #Associate pin 26 to pir
-GPIO.setup(pir, GPIO.IN)                          #Set pin as GPIO in 
+GPIO.setmode(GPIO.BOARD)                          #Bring up Raspberry PI GPIO pins
+pir = 26                                          #Set this value to whichever pin is listening to the sensor
+GPIO.setup(pir, GPIO.IN)                          
 print "HALLOWEEN LIGHTS - Waiting for sensor to settle"
-time.sleep(2)                                     #Waiting 2 seconds for the sensor to initiate
+time.sleep(2)                                     #Give the sensor 2 sec. to come online
 print "Detecting motion"
-while True:
-	if GPIO.input(pir):                            #Check whether pir is HIGH
+while True:					  #Loop forever
+	if GPIO.input(pir):                            #Check pin
       		print "Motion Detected!"
 	      	print (datetime.datetime.now())
 
@@ -72,8 +76,8 @@ while True:
 	
 		# for k
 
-	time.sleep(2)                               #D1- Delay to avoid multiple detection
-	time.sleep(0.1)                                #While loop delay should be less than detection(hardware) delay
+	time.sleep(2)                               #Delay for 2 sec. to avoid false, double detection
+	time.sleep(0.1)                              
 
 
 
